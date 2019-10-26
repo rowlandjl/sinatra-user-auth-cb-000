00d8af6e39423d473af0dabc725fa0c5a8a1bc9e
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-26 01:38:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id 
       redirect '/users/home'
     end 
-    redirect 
+    redirect '/sessions/login'
   end
 
   get '/sessions/logout' do 
